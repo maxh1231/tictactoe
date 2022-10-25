75207b89109dbc2ef2b90ecc667089b491c0188b
maxh1231 <maxhumpherys@gmail.com>
2022-10-25 14:46:33 -0600

    added react router package with routes, socket.io client side react setup

--- client/src/App.js
+++ client/src/App.js
@@ -8,25 +8,36 @@ import {
   Link
 } from "react-router-dom";
 
+// Pages
+import Menu from './pages/Menu';
+
 function App() {
   return (
     <Router>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+
+      {/* <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div> */}
+
+      <Switch>
+        <Route path="/">
+          <Menu />
+        </Route>
+      </Switch>
+
     </Router>
   );
 }
--- client/src/pages/Menu.js
+++ client/src/pages/Menu.js
@@ -1,7 +1,31 @@
+import io from 'socket.io-client';
+
 const Menu = () => {
+    var socket = io();
+    var messages = document.getElementById('messages');
+    var form = document.getElementById('form');
+    var input = document.getElementById('input');
+
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+        if (input.value) {
+            socket.emit('chat message', input.value);
+            input.value = '';
+        }
+    });
+
     return (
-        <section></section>
+        <section>
+            <h1>hello!!!!!!!!!!</h1>
+            <ul id="messages"></ul>
+            <form id="form" action="">
+                <input id="input" autocomplete="off" /><button>Send</button>
+            </form>
+
+
+        </section>
     )
 }
+
 
 export default Menu;

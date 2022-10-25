983e69217f83a24c6da4bb49949e3bf166139063
maxh1231 <maxhumpherys@gmail.com>
2022-10-24 21:06:50 -0600

    basic express setup

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.json({ message: 'hello from server' })
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+    });
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});

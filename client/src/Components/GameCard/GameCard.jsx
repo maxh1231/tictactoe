ff5e2587f7bf4a99376c09f2af4bea2097da60dd
maxh1231 <maxhumpherys@gmail.com>
2022-10-28 19:34:14 -0600

    created function to add player 1's mark

--- client/src/Components/GameCard/GameCard.jsx
+++ client/src/Components/GameCard/GameCard.jsx
@@ -4,10 +4,22 @@ import icon_x from '../../assets/icon-x.svg'
 import icon_restart from '../../assets/icon-restart.svg'
 import logo from '../../assets/logo.svg'
 import { useLocation } from 'react-router'
+import { useState } from 'react'
 
 const GameCard = () => {
+    const [active, setActive] = useState(null);
     const location = useLocation();
     console.log(location.state)
+
+    const makeMove = (event) => {
+        if (location.state.mark === 'X') {
+            event.target.children[0].setAttribute('src', icon_x)
+        }
+        if (location.state.mark === 'O') {
+            event.target.children[0].setAttribute('src', icon_o);
+        }
+    }
+
     return (
         <div className='flex flex-col'>
             <div className='flex justify-between'>
@@ -23,9 +35,15 @@ const GameCard = () => {
             </div>
 
             <div className="flex my-4">
-                <div className='w-[140px] h-[140px] bg-[#1F3641] rounded-xl mx-2'></div>
-                <div className='w-[140px] h-[140px] bg-[#1F3641] rounded-xl mx-2'></div>
-                <div className='w-[140px] h-[140px] bg-[#1F3641] rounded-xl mx-2'></div>
+                <div className='w-[140px] h-[140px] bg-[#1F3641] rounded-xl mx-2' onClick={makeMove}>
+                    <img src=''></img>
+                </div>
+                <div className='w-[140px] h-[140px] bg-[#1F3641] rounded-xl mx-2'>
+                    <img src=''></img>
+                </div>
+                <div className='w-[140px] h-[140px] bg-[#1F3641] rounded-xl mx-2'>
+                    <img src=''></img>
+                </div>
             </div>
             <div className="flex my-4">
                 <div className='w-[140px] h-[140px] bg-[#1F3641] rounded-xl mx-2'></div>

e83ee06184c58f13209b44242c33041b2ff0e45a
maxh1231 <maxhumpherys@gmail.com>
2022-11-03 17:27:02 -0600

    created conditional SVGS for players turn

--- client/src/Components/GameCard/GameCard.jsx
+++ client/src/Components/GameCard/GameCard.jsx
@@ -62,12 +62,16 @@ const GameCard = () => {
 
     return (
         <div className='flex flex-col'>
-            <div className='flex justify-between'>
+            <div className='flex justify-between mx-2'>
                 <div>
-                    <img src={logo}></img>
+                    <img className='logo' src={logo}></img>
                 </div>
-                <div>
-                    {activeMark === 'X' ? <span>X TURN</span> : <span>O TURN</span>}
+                <div className='flex flex-row justify-center items-center w-[140px] h-[40px] rounded-xl bg-[#1F3641]'>
+                    {activeMark === 'X' ?
+                        <span className='text-sm text-[#A8BFC9]'><svg width="64" height="64" className='game-icon' xmlns="http://www.w3.org/2000/svg"><path d="M15.002 1.147 32 18.145 48.998 1.147a3 3 0 0 1 4.243 0l9.612 9.612a3 3 0 0 1 0 4.243L45.855 32l16.998 16.998a3 3 0 0 1 0 4.243l-9.612 9.612a3 3 0 0 1-4.243 0L32 45.855 15.002 62.853a3 3 0 0 1-4.243 0L1.147 53.24a3 3 0 0 1 0-4.243L18.145 32 1.147 15.002a3 3 0 0 1 0-4.243l9.612-9.612a3 3 0 0 1 4.243 0Z" fill="#A8BFC9" fill-rule="evenodd" /></svg>TURN</span>
+                        :
+                        <span className='text-sm text-[#A8BFC9]'><svg width="64" height="64" className='inline game-icon' xmlns="http://www.w3.org/2000/svg"><path d="M32 0c17.673 0 32 14.327 32 32 0 17.673-14.327 32-32 32C14.327 64 0 49.673 0 32 0 14.327 14.327 0 32 0Zm0 18.963c-7.2 0-13.037 5.837-13.037 13.037 0 7.2 5.837 13.037 13.037 13.037 7.2 0 13.037-5.837 13.037-13.037 0-7.2-5.837-13.037-13.037-13.037Z" fill="#A8BFC9" /></svg>TURN</span>
+                    }
                 </div>
                 <div>
                     <img src={icon_restart}></img>

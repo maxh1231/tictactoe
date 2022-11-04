969811debdacd480dec816852120b06f04c9bbca
maxh1231 <maxhumpherys@gmail.com>
2022-11-03 18:12:45 -0600

    commit

--- client/src/Components/GameCard/GameCard.jsx
+++ client/src/Components/GameCard/GameCard.jsx
@@ -63,7 +63,7 @@ const GameCard = () => {
     return (
         <div className='flex flex-col'>
             <div className='flex justify-between mx-2'>
-                <div>
+                <div className=''>
                     <img className='logo' src={logo}></img>
                 </div>
                 <div className='flex justify-evenly items-center w-[140px] h-[40px] rounded-xl bg-[#1F3641]'>
@@ -73,8 +73,8 @@ const GameCard = () => {
                         <span className='text-sm text-[#A8BFC9]'><svg width="64" height="64" className='inline game-icon' xmlns="http://www.w3.org/2000/svg"><path d="M32 0c17.673 0 32 14.327 32 32 0 17.673-14.327 32-32 32C14.327 64 0 49.673 0 32 0 14.327 14.327 0 32 0Zm0 18.963c-7.2 0-13.037 5.837-13.037 13.037 0 7.2 5.837 13.037 13.037 13.037 7.2 0 13.037-5.837 13.037-13.037 0-7.2-5.837-13.037-13.037-13.037Z" fill="#A8BFC9" /></svg>TURN</span>
                     }
                 </div>
-                <div>
-                    <img src={icon_restart}></img>
+                <div className='flex items-center bg-[#A8BFC9] p-3 rounded-xl'>
+                    <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg"><path d="M19.524 0h-1.88a.476.476 0 0 0-.476.499l.159 3.284A9.81 9.81 0 0 0 9.835.317C4.415.317-.004 4.743 0 10.167.004 15.597 4.406 20 9.835 20a9.796 9.796 0 0 0 6.59-2.536.476.476 0 0 0 .019-.692l-1.348-1.349a.476.476 0 0 0-.65-.022 6.976 6.976 0 0 1-9.85-.63 6.987 6.987 0 0 1 .63-9.857 6.976 6.976 0 0 1 10.403 1.348l-4.027-.193a.476.476 0 0 0-.498.476v1.881c0 .263.213.476.476.476h7.944A.476.476 0 0 0 20 8.426V.476A.476.476 0 0 0 19.524 0Z" fill="#1F3641" /></svg>
                 </div>
             </div>
 
